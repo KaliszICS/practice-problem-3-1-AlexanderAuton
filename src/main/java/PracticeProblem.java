06719e198564fe2a7e7af601d0eaab2e425c8e1a
AlexanderAuton <350319867@tdsb.ca>
2026-04-15 15:23:07 +0000

    neverGonnaGiveYouUpNeverGonnaLetYouDown

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,42 @@
-public class PracticeProblem {
+
+/**
+
+        * File: Lesson 3.1 - if
+
+        * Author: A.Auton
+
+        * Date Created: March 23, 2026
+
+        * Date Last Modified: April 15, 2026
+
+        */
+	   public class PracticeProblem {
 
 	public static void main(String args[]) {
-
 	}
 
+	public static String evenOrOdd(int num)
+	 {
+if (num%2 == 0) {
+	return "Even";
+}
+return "Odd";		
+}
+public static String teacherOrStudent(String s)
+	{
+if (s == "Kalisz"){
+		return "Teacher";
+}
+return "Student";
+	}
+public static int fartherFromZero(int num) 
+{
+	if (num > 0){
+		return (num + 5);
+	}
+	if (num < 0){
+		return (num - 5);
+	}
+	return 0;
+}
 }
